a28c9b8d2a91d958d31a80fdc805740f68db1a6c
francescobruschetti <francesco.bruschetti@studenti.unitn.it>
2018-01-12 10:42:38 +0100

    Server base

--- astronauts.js
+++ astronauts.js
@@ -11,15 +11,5 @@ astronauts.get('/client', function (req, res) {
 
 })
 
-astronauts.get('/:astronautsID', function (req, res) {
-
-    const astronautsID = req.params.astronautsID
-    const i = astronautsDetails.findIndex(item => {return item.astronautsID === astronautsID})
-    if (i==-1) res.sendStatus(404)
-    else {
-        res.status=200
-        res.json(astronautsDetails[i])
-    }
-})
 
 module.exports = astronauts
