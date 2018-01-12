b8af31332943ffe60160426415d02a3f49a9bf56
francescobruschetti <francesco.bruschetti@studenti.unitn.it>
2018-01-12 11:15:08 +0100

    Add new branch 'put' with function for update astronaut's details

--- astronauts.js
+++ astronauts.js
@@ -6,32 +6,15 @@ var uuid = require('uuid-v4');
 
 const astronautsDetails = []
 
-astronauts.get('/client', function (req, res) {
-    res.sendfile("client.html");
-
-})
-
-astronauts.get('/', function (req, res) {
-    res.json(astronautsDetails)
-})
-
-astronauts.post('/', function (req, res) {
-	
-    const newAstronaut = req.body
-    newAstronaut.astronautID = uuid()
-    astronautsDetails.push(newAstronaut)
-    res.json(newAstronaut)
-})
-
-astronauts.get('/:astronautID', function (req, res) {
+astronauts.put('/:astronautID', function (req, res) {
 
     const astronautID = req.params.astronautID
     const i = astronautsDetails.findIndex(item => {return item.astronautID === astronautID})
-    if (i==-1) res.sendStatus(404)
-    else {
-        res.status=200
-        res.json(astronautsDetails[i])
-    }
+	
+	console.log(req.body);
+	
+    astronautsDetails[i] = req.body
+    res.json(astronautsDetails[i])
 })
 
 module.exports = astronauts
