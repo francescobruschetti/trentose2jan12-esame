f925338feaa8ba1fcff4d63509b8432b12bc9ffa
francescobruschetti <francesco.bruschetti@studenti.unitn.it>
2018-01-12 10:32:18 +0100

    Add new branch simpleget

--- astronauts.js
+++ astronauts.js
@@ -6,22 +6,22 @@ var uuid = require('uuid-v4');
 
 const astronautsDetails = []
 
-
 astronauts.get('/client', function (req, res) {
     res.sendfile("client.html");
 
 })
 
-// funzione che restituisce tutti gli astronauti e i loro dettagli
 astronauts.get('/', function (req, res) {
     res.json(astronautsDetails)
 })
 
-// funzione con cui caricare un nuovo astronauta
 astronauts.post('/', function (req, res) {
 	
+	console.log(req.body.firstName)
     const newAstronaut = req.body
     newAstronaut.ID = uuid()
     astronautsDetails.push(newAstronaut)
     res.json(newAstronaut)
 })
+
+module.exports = astronauts
