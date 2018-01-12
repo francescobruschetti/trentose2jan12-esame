dbfa89aa7e36dbb05acd3c2652e2113140d7c3e4
francescobruschetti <francesco.bruschetti@studenti.unitn.it>
2018-01-12 10:44:44 +0100

    Update astronauts file in branch

--- astronauts.js
+++ astronauts.js
@@ -6,20 +6,17 @@ var uuid = require('uuid-v4');
 
 const astronautsDetails = []
 
-astronauts.get('/client', function (req, res) {
-    res.sendfile("client.html");
 
+astronauts.get('/', function (req, res) {
+    res.json(astronautsDetails)
 })
 
-astronauts.get('/:astrinautsID', function (req, res) {
-
-    const astrinautsID = req.params.astrinautsID
-    const i = astronautsDetails.findIndex(item => {return item.astrinautsID === astrinautsID})
-    if (i==-1) res.sendStatus(404)
-    else {
-        res.status=200
-        res.json(astronautsDetails[i])
-    }
+astronauts.post('/', function (req, res) {
+	
+    const newAstronaut = req.body
+    newAstronaut.astronautID = uuid()
+    astronautsDetails.push(newAstronaut)
+    res.json(newAstronaut)
 })
 
 module.exports = astronauts
